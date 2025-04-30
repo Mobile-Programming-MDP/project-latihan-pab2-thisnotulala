8f0cb5cf71f1f8b729448e2ab4388dd355d95937
Cesya Catherine <cesyacatherine_2226240018@mhs.mdp.ac.id>
2025-04-30 18:30:59 +0700

    commit post gambar
    commit post gambar

--- fasum/lib/screens/sign_up_screen.dart
+++ fasum/lib/screens/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _fullNameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
@@ -25,6 +27,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextField(
+              controller: _fullNameController,
+              decoration: const InputDecoration(labelText: 'Full Name'),
+            ),
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(labelText: 'Email'),
@@ -66,10 +72,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
 
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
+      final newUser = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text);
+
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(newUser.user!.uid)
+          .set({
+        'fullName': _fullNameController.text,
+        'email': _emailController.text,
+        'createdAt': Timestamp.now()
+      });
+
       if (mounted) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
